6143f4dfed4af329b3e2919b6efa7c2fe1b58bfd
Paul Tan <pjxtan@gmail.com>
2017-06-26 19:15:01 -0400

    Add project files.

---
+++ Chino/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chino.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
