9c8d71ccc0e906d8b94f892906e8e4a0dbd7145e
Paul Tan <pjxtan@gmail.com>
2017-06-30 00:51:35 -0400

    Reworked layout to use tabs; added some drag/drop handling for tags

--- Chino/MainWindow.xaml.cs
+++ Chino/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using Chino.ViewModel;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System;
+using MaterialDesignThemes.Wpf;
 
 namespace Chino
 {
@@ -9,6 +12,8 @@ namespace Chino
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Point _mouseStartPoint;
+
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
         /// </summary>
@@ -30,6 +35,28 @@ namespace Chino
 
             var viewModel = (MainViewModel)this.DataContext;
             viewModel.SelectedTagFilter = button.Content.ToString();
+        }
+
+        private void tagChip_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _mouseStartPoint = e.GetPosition(null);
+        }
+
+        private void tagChip_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point mousePoint = e.GetPosition(null);
+            Vector dist = _mouseStartPoint - mousePoint;
+
+            if (e.LeftButton == MouseButtonState.Pressed &&
+                Math.Abs(dist.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                Math.Abs(dist.Y) > SystemParameters.MinimumVerticalDragDistance)
+            {
+                Chip tagChip = sender as Chip;
+                var tag = tagChip.Content.ToString();
+
+                var dragData = new DataObject(DataFormats.StringFormat, tag);
+                DragDrop.DoDragDrop(tagChip, dragData, DragDropEffects.Copy);
+            }
         }
     }
 }
